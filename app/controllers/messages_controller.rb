dcf6dcb7d9eab75d5ea019efe1ee0e02cadab8f0
Derek <derekweatherford@gmail.com>
2015-12-21 23:14:04 -0700

    it fucking works!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,39 @@
 class MessagesController < ApplicationController
   include ActionController::Live
 
+
+  def initialize
+    @speed = 0
+  end
+
   def serve
-    listener = SpeedListener.new
+    # listen
+
+    x = Thread.new do 
+      x["speed"] = 0
+      puts "SpeedListener is running!"
+      $redis.subscribe("speed") do |event|
+        puts 'block running'
+        event.message do |channel, body|
+          puts "#{body}"
+          x["speed"] = body   
+
+        end
+      end
+      ActiveRecord::Base.connection.close
+      puts 'closed'
+    end
+
     puts 'serving messages'
     response.headers['Content-Type'] = 'text/event-stream'
-
       sse = SSE.new(response.stream)
       begin
         loop do 
           # puts "#{listener.speed}"
-          # sse.write("data: #{listener.speed}") 
-          sse.write("data: 1") 
-
-          sleep 3           
+          # sse.write("data: #{@speed}") 
+          puts x[:speed]
+          sse.write("data: #{x[:speed]}") 
+          sleep 1           
         end 
       rescue IOError
         # Client Disconnected
@@ -22,4 +42,23 @@ class MessagesController < ApplicationController
       end
     render nothing: true       
   end
+
+  def listen
+    Thread.new do 
+      puts "SpeedListener is running!"
+      $redis.subscribe("speed") do |event|
+        puts 'block running'
+        event.message do |channel, body|
+          puts "#{body}"
+          @speed = body        
+        end
+      end
+      ActiveRecord::Base.connection.close
+      puts 'closed'
+    end
+  end
 end
+
+
+
+
